37c7ba842f9309b349153a5591e0a291808bfce0
Sanhee <68312957+sanheekim@users.noreply.github.com>
2025-02-08 21:51:19 +0900

    [Bronze V] Title: ??!, Time: 168 ms, Memory: 17668 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        String id = sc.next();
+        String lastWord = "??!";
+        System.out.println(id+lastWord);
+    }
+}
