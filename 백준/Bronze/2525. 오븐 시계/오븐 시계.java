d3ddad26880fafe63f928ef0d62d2f8869e714b8
Sanhee <68312957+sanheekim@users.noreply.github.com>
2025-02-19 13:36:21 +0900

    [Bronze III] Title: 오븐 시계, Time: 192 ms, Memory: 18396 KB -BaekjoonHub

--- 백준/Bronze/2525. 오븐 시계/오븐 시계.java
+++ 백준/Bronze/2525. 오븐 시계/오븐 시계.java
@@ -1,17 +1,17 @@
 import java.util.*;
 
-public class Main {
+public class Main{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
         int c = sc.nextInt();
+        
         sc.close();
         
-        int total = 60*a+b;
-        total = total + c;
-        int hour = (total/60)%24;
-        int min = total%60;
+        int min = 60*a+b+c;
+        int hour = (min/60)%24;
+        min = min%60;
         
         System.out.println(hour+" "+min);
     }
