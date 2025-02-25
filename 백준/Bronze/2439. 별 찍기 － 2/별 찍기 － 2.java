9d6de3f212974514302602ba1b58d43c4b1eba7e
Sanhee <68312957+sanheekim@users.noreply.github.com>
2025-02-25 18:25:32 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 308 ms, Memory: 22400 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int max = sc.nextInt();
+        
+        for (int i=1; i<=max; i++){
+            for (int j=1; j<=max-i; j++){
+                System.out.print(" ");
+            }
+            for (int k=1; k<=i; k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        
+        sc.close();
+    }
+}
