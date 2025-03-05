cc6e48323b3dd422e83d41f67bfb8513be21c2c7
Sanhee <68312957+sanheekim@users.noreply.github.com>
2025-03-05 20:28:50 +0900

    [Bronze V] Title: 개수 세기, Time: 180 ms, Memory: 17824 KB -BaekjoonHub

--- 백준/Bronze/10807. 개수 세기/개수 세기.java
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -12,7 +12,7 @@ public class Main {
         
         int count = 0;
         int cnt = sc.nextInt();
-        for(int j=0;j<arr.length;j++){
+        for(int j=0;j<max;j++){
             if (cnt==arr[j]){
                 count++;
             }
@@ -21,19 +21,3 @@ public class Main {
         System.out.println(count);
     }
 }
-
-
-/*
-첫째 줄에 정수의 개수 N(1 ≤ N ≤ 100)이 주어진다.
-둘째 줄에는 정수가 공백으로 구분되어져있다.
-셋째 줄에는 찾으려고 하는 정수 v가 주어진다.
-입력으로 주어지는 정수와 v는 -100보다 크거나 같으며, 100보다 작거나 같다.
-
-입력
-11
-1 4 1 2 4 2 4 2 3 4 4
-2
-
-출력
-3
-*/
