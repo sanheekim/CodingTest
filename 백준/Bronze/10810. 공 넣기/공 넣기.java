059fa6dcd2e535e0b0cf7c4ecd78ad1bb1c1dd05
Sanhee <68312957+sanheekim@users.noreply.github.com>
2025-03-16 16:58:58 +0900

    [Bronze III] Title: 공 넣기, Time: 204 ms, Memory: 18688 KB -BaekjoonHub

--- 백준/Bronze/10810. 공 넣기/공 넣기.java
+++ 백준/Bronze/10810. 공 넣기/공 넣기.java
@@ -6,14 +6,13 @@ public class Main {
         int N = sc.nextInt();
         int[] arr = new int[N];
         int M = sc.nextInt();
-        
         for(int i=0;i<M;i++){
             int I = sc.nextInt();
             int J = sc.nextInt();
             int K = sc.nextInt();
             
             for (int j=I-1;j<J;j++){
-                arr[j]=K;
+                arr[j] = K;
             }
         }
         for(int k=0;k<N;k++){
