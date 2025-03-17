c9dc1777f326afbb175fed3bbb3342598e26f647
Sanhee <68312957+sanheekim@users.noreply.github.com>
2025-03-18 07:46:47 +0900

    [Bronze II] Title: 나머지, Time: 168 ms, Memory: 17692 KB -BaekjoonHub

--- 백준/Bronze/3052. 나머지/나머지.java
+++ 백준/Bronze/3052. 나머지/나머지.java
@@ -1,14 +1,13 @@
 import java.util.*;
 
-public class Main{
+public class Main {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int[] arr = new int[10];
         HashSet<Integer> h = new HashSet<Integer>();
-        
         for(int i=0;i<arr.length;i++){
-            int num = sc.nextInt();
-            h.add(num%42);
+            int yes = sc.nextInt();
+            h.add(yes%42);
         }
         System.out.println(h.size());
     }
