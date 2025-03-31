fe5b3794b07af81086227618f734d932971f6d92
Sanhee <68312957+sanheekim@users.noreply.github.com>
2025-03-31 16:16:40 +0900

    [Bronze II] Title: 상수, Time: 104 ms, Memory: 14080 KB -BaekjoonHub

--- 백준/Bronze/2908. 상수/상수.java
+++ 백준/Bronze/2908. 상수/상수.java
@@ -3,16 +3,12 @@ import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        try {
-            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         
-            int A = Integer.parseInt(new StringBuilder(st.nextToken()).reverse().toString());
-            int B = Integer.parseInt(new StringBuilder(st.nextToken()).reverse().toString());
+        int a = Integer.parseInt(new StringBuilder(st.nextToken()).reverse().toString());
+        int b = Integer.parseInt(new StringBuilder(st.nextToken()).reverse().toString());
         
-            System.out.print(A>B?A:B);
-            } catch (IOException e) {
-            e.printStackTrace();
-        }
+        System.out.println(a>b?a:b);
     }
 }
