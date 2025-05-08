5b7752d2b0ac7a09548f3554c6fd8465d2c2b7bd
Sanhee <68312957+sanheekim@users.noreply.github.com>
2025-05-08 18:03:48 +0900

    [Bronze III] Title: 배수와 약수, Time: 160 ms, Memory: 17632 KB -BaekjoonHub

--- 백준/Bronze/5086. 배수와 약수/배수와 약수.java
+++ 백준/Bronze/5086. 배수와 약수/배수와 약수.java
@@ -2,14 +2,17 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
         
+        Scanner sc = new Scanner(System.in);        
         while(true){
             
             int a = sc.nextInt();
             int b = sc.nextInt();
             
-            if (a==0&&b==0) break;
+            if(a==0 && b==0){
+                break;
+            }
+            
             if (b%a==0){
                 System.out.println("factor");
             } else if (a%b==0){
@@ -17,6 +20,10 @@ public class Main {
             } else {
                 System.out.println("neither");
             }
+            
         }
     }
 }
+
+//각 테스트 케이스마다
+//첫 번째 숫자가 두 번째 숫자의 약수라면 factor를, 배수라면 multiple을, 둘 다 아니라면 neither를 출력한다.
